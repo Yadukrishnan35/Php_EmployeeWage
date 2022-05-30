62c5055a4cc35192dfc95371d6656c0e8fa45036
yadukrishnan <yadulive333@gmail.com>
2022-05-30 19:52:59 +0530

    Added: UC1-Employee Attendence Check

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -4,8 +4,20 @@ class EmpWages{
    function welcomeMsg(){
        echo "Welcome to employee wage problem"."\n";
    }
+   function empCheck(){
+      $number =rand(0,1);
+       if($number==1){
+           echo "Employee is present";
+       }
+       else{
+           echo "Employee is Absent";
+       }
+
+   }
 }
 $empWage = new EmpWages();
 $empWage->welcomeMsg();
+$AttendanceCheck = new EmpWages();
+$AttendanceCheck->empCheck();
 
 ?>
